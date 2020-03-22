babadb96b0010c964c55b42513eb1e30da199b0d
allen <324734127@qq.com>
2020-03-22 21:33:23 +0800

    first commit

---
+++ sbt-security/src/main/java/com/fishdemon/sbt/security/Application.java
@@ -0,0 +1,17 @@
+package com.fishdemon.sbt.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring secuity demo
+ * @author Anjin.Ma
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
